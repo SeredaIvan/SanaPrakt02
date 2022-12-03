ce3e563e746e6c9a587f216f2fb08a927cf193be
SeredaIvan <vt221_bio@student.ztu.edu.ua>
2022-12-03 21:11:49 +0200

    zavd18

--- SanaPrakt02/task1/Program.cs
+++ SanaPrakt02/task1/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("довільний рядок 1");
+Console.WriteLine("довільний рядок 2");
+Console.WriteLine("довільний рядок 3");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
